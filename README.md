210eea3d9d67867864abf19e6d5945c5fe6f2c02
saikiran-gutla <saikiran.gutla183@gmail.com>
2019-11-12 23:39:46 +0530

    EXam and videos module set

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Learn-Vault2
+# BackEnd Setup
+
+git clone https://github.com/saikiran-gutla/Student_Management.git
+
+python3 -m venv your_env_name
+
+. .your_env_name/bin/activate or source your_env_name/bin/activate
+
+# Requirements Istallation
+
+pip install -r requirements.txt
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py createsuperuser
+
+username:- schl_mngt
+password:- schl123
+
+python manage.py runserver
+
+
---
+++ schl_app/questions.py
@@ -0,0 +1,42 @@
+questions_list = [
+    {
+        'q_no': 'q1',
+        'lno': 'l1',
+        'question': 'Which of the following data types is not supported in python?',
+        'answer': 'Slice',
+        'choices': ['Numbers', 'String', 'List', 'Slice'],
+        'hint': 'first question hint',
+    },
+    {
+        'q_no': 'q2',
+        'lno': 'l2',
+        'question': 'Which of the data types is not supported in python?',
+        'answer': 'Generics',
+        'choices': ['Tuple', 'Dictionary', 'Generics', 'List'],
+        'hint': 'second question hint'
+    },
+    {
+        'q_no': 'q3',
+        'lno': 'l3',
+        'question': 'What command is used to shuffle a list ‘L’?',
+        'answer': 'random.Shuffle(L)',
+        'choices': ['L.shuffle()', 'random.shufflelist(L)', 'shuffle(L)', 'random.Shuffle(L)'],
+        'hint': 'third question hint'
+    },
+    {
+        'q_no': 'q4',
+        'lno': 'l4',
+        'question': "What is the HTML tag under which one can write the JavaScript code?",
+        'answer': '<script>',
+        'choices': ['<javascript>', '<scripted>', '<script>', '<js>'],
+        'hint': 'Fourth question hint'
+    },
+    {
+        'q_no': 'q5',
+        'lno': 'l5',
+        'question': 'HTML is?',
+        'answer': 'MarkUp Language',
+        'choices': ['Scripting Language', 'Programming Language', 'MarkUp Language', 'None of the Above'],
+        'hint': 'fifth question hint'
+    }
+]
